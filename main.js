287b7c33c9742dde0ed743d854ff008e2d3faa56
facusteckler86 <facundomsteckler@gail.com>
2023-02-02 00:18:08 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,43 @@
+//comentario de linea
+
+/* 
+declaracion de variable
+*/
+let edad = 45;
+// var nombre ="Andres"; //ya no se usa mucho, es codigo viejo
+const NUMERO = 10; // se declara con mayuscula
+
+// let nombre = "Facundo";
+// console.log("hola", nombre);//comando importante no olvidar
+
+// nombreBonitoGrande // camel case
+// nombre_Bonito_Grande //snake case
+// NombreBonitoGrande // pascal case
+
+// let numeroUno = 4;
+// let numeroDos = 5;
+// let resultadoSuma = numeroUno + numeroDos;
+// let resultadoResta = numeroUno - numeroDos;
+// let resultadoMultiplicacion = numeroUno * numeroDos;
+// let resultadoDivision = numeroUno / numeroDos;
+
+// console.log("resulatado de la suma" , resultadoSuma);
+// console.log("resulatado de la resta" , resultadoResta);
+// console.log("resulatado de la multiplicacion", resultadoMultiplicacion);
+// console.log("resulatado de la division", resultadoDivision);
+
+// let saludo = "Hola";
+// let nombre = "Facundo";
+
+// console.log(mensaje);
+// // console.log("Hola" + 1);
+
+// let mensaje = `este es el saludo ${saludo} ${nombre}`;
+
+// prompt("Ingrese su nombre");
+// alert (nombre);
+let nombre = parseFloat (prompt("Diga su nombre: "))
+let edad = parseInt(prompt("Ingrese su Edad: "));
+
+
+alert (`Usted es  ${nombre} ${edad}`);
