1357ad9ee97411ee8e2b2922e226e30253e7997b
facusteckler86 <facundomsteckler@gail.com>
2023-02-08 21:50:44 -0300

    pueba de bucles

--- main.js
+++ main.js
@@ -3,9 +3,9 @@
 /* 
 declaracion de variable
 */
-let edad = 45;
-// var nombre ="Andres"; //ya no se usa mucho, es codigo viejo
-const NUMERO = 10; // se declara con mayuscula
+// let edad = 45;
+// // var nombre ="Andres"; //ya no se usa mucho, es codigo viejo
+// const NUMERO = 10; // se declara con mayuscula
 
 // let nombre = "Facundo";
 // console.log("hola", nombre);//comando importante no olvidar
@@ -36,8 +36,52 @@ const NUMERO = 10; // se declara con mayuscula
 
 // prompt("Ingrese su nombre");
 // alert (nombre);
-let nombre = parseFloat (prompt("Diga su nombre: "))
-let edad = parseInt(prompt("Ingrese su Edad: "));
+// let nombre = parseFloat (prompt("Diga su nombre: "))
+// let edad = parseInt(prompt("Ingrese su Edad: "));
 
 
-alert (`Usted es  ${nombre} ${edad}`);
+// alert (`Usted es  ${nombre} ${edad}`);
+
+// let nombre = "andres";
+
+// if(nombre){
+//     console.log("Hola Entramos al if");
+// }
+
+// let fideos = prompt ("Vas a comer fideos?");
+// let milanesas = prompt ("vas a comer milanesas?");
+
+// if (fideos === "si" && milanesas === "si"){
+//     alert ("Avisa si vas a comer fideos")
+// } else if (fideos === "No" && milanesas === "si"){
+//     alert (" Vas a comer milanesas entonces?")
+   
+// } else if (fideos === "No" && milanesas === "No"){
+//     alert ("Que bueno que cerraste la boca");
+// }
+
+let entrada = prompt("Ingrese un numero");
+
+for(let i = 1; <= 10; i++){
+
+    let resultado = numero + i; 
+    
+    
+    while (entrada != "Esc"){
+        
+        alert (`${numero} + ${i} = ${resultado}`)
+    switch (entrada) {
+        case "uno":
+            alert ("ingreso el uno")
+            break;
+        case "dos":
+            alert ("ingreso el dos")
+        case "tres":
+            alert ("ingreso el tres")
+    
+        default:
+            alert("ustes agrego los numeros uno, dos y tres")
+            break;
+    }
+}
+}
